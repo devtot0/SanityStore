a2ba180689b322d578d7c644d3ef25edd94825f2
User <lifesim11@gmail.com>
2024-01-12 14:16:41 +0100

    added shopping cart modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import CartProvider from "@/components/Providers";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <CartProvider>
+          <Navbar />
+          {/* <ShoppingCartModal /> */}
+          {children}
+        </CartProvider>
         {children}
       </body>
     </html>
